73459347b161995152da8528ec9461e5054f7250
Quanyi Ma <eli@patch.sh>
2025-10-23 15:32:01 +0800

    Add Google Analytics
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Inter } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -12,6 +13,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
+      <GoogleAnalytics gaId="G-LS2KMQZ86F" />
     </html>
   );
 }
