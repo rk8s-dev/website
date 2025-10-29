9daf398ea017a073fead47eb747f99ea79fc9c9c
Quanyi Ma <eli@patch.sh>
2025-10-29 20:22:02 +0800

    Add logo files
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 /**
  * Shared layout configurations
@@ -12,14 +13,13 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
+          <Image
+            src="/images/logo/logo-blue.svg"
+            width={24}
+            height={24}
+            alt="Logo"
+            className="mr-2"
+          />
           RK8S
         </>
       ),
