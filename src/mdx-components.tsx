b3cab2ff9e445a69c428c8a2229f698e25e10775
jackieismpc <jackieismpc@gmail.com>
2026-03-27 14:54:43 +0800

    update dagrs 0.8.1
    Signed-off-by: jackieismpc <jackieismpc@gmail.com>

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,15 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { Callout } from 'fumadocs-ui/components/callout';
+import { Card, Cards } from 'fumadocs-ui/components/card';
 import type { MDXComponents } from 'mdx/types';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Callout,
+    Card,
+    Cards,
     ...components,
   };
 }
